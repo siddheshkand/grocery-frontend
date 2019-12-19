2816a5c21db63f1a1384f55cec4903496ba3f364
siddkand <siddkand@gmail.com>
2019-12-20 00:16:12 +0530

    Material design added

---
+++ src/app/header/user-nav/user-nav.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-user-nav',
+  templateUrl: './user-nav.component.html',
+  styleUrls: ['./user-nav.component.css']
+})
+export class UserNavComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
